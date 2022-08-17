82f4b4d27eb1141896777a19d11141c69ccade57
iijimakota <kotaiijima@icloud.com>
2022-08-17 18:59:23 +0900

    fix count_shift

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: kiijima <kiijima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 12:17:30 by kiijima           #+#    #+#             */
-/*   Updated: 2022/07/26 20:53:42 by kiijima          ###   ########.fr       */
+/*   Updated: 2022/08/17 18:54:06 by kiijima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ void	send_msg(int pid, char *msg)
 		shift_count = 0;
 		while (shift_count < 8)
 		{
-			if (msg[i] >> shift_count & 0b00000001)
+			if ((msg[i] >> shift_count) & 0b00000001)
 				kill_status = kill(pid, SIGUSR1);
 			else
 				kill_status = kill(pid, SIGUSR2);
 			if (kill_status == -1)
 				error_msg("kill error");
 			shift_count++;
-			usleep(10000);
+			usleep(500);
 		}
 		i++;
 	}
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: kiijima <kiijima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 12:18:37 by kiijima           #+#    #+#             */
-/*   Updated: 2022/07/26 18:20:45 by kiijima          ###   ########.fr       */
+/*   Updated: 2022/08/17 18:58:28 by kiijima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,24 @@ void	put_pid(void)
 void	put_character(int signal)
 {
 	if (signal == SIGUSR1)
-		g_signal.character += (0b100000000 >> g_signal.count);
-	if (g_signal.count == 1)
+		g_signal.character += (0b00000001 << g_signal.count);
+	if (g_signal.count == 8)
 	{
 		ft_putchar_fd(g_signal.character, STDOUT_FILENO);
 		g_signal.character = 0;
-		g_signal.count = 8;
+		g_signal.count = 0;
 		return ;
 	}
-	g_signal.count--;
+	g_signal.count++;
 }
 
 int	main(void)
 {
 	g_signal.character = 0;
-	g_signal.count = 8;
+	g_signal.count = 0;
 	put_pid();
 	signal(SIGUSR1, put_character);
 	signal(SIGUSR2, put_character);
-	while (1)
+	while(1)
 		pause();
 }
